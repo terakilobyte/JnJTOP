3de60b309bb628dd4c17d06ebe943154cb9fcbe0
tkbyte <mellicap@outlook.com>
2014-11-28 22:48:15 +0900

    Tidying up

--- js/script.js
+++ js/script.js
@@ -1,51 +1,54 @@
-$(document).ready(function() {
-  generatePallet(16);
-  etch();
-  resize();
-  
-  // Discovered that when things are dynamically made, any interactions on them must be put inside callable functions!
-  function etch() {
-    $('.normalDiv').hover(function() {
-      $(this).addClass('hoverDiv');
-      $(this).removeClass('normalDiv');
-    },
-    function() {
-      // do nothing on mouse exit
-    });
-  }
+/*jslint browser: true, devel: true, plusplus: true, space: true, latedef: nofunc*/
+/*global $, jQuery, alert*/
 
-  function resize() {
-    $('#resize button').click(function() {
-      var newSide = parseInt(prompt("Specify the new width in squares you want to draw in. Remember, total area is width*height"));
-      clearPallet(newSide);
-    });
-  }
-  
-  
-  function generatePallet (side) {
-    var divLimit = 960;
-    $('body').prepend($('<div id="resize"><button>Change Pixel Density</button></div>'));
-    $('#resize').after($('<div id="container"></div>'));
-    for (var i = 1; i <= side; i++) {
-      for (var j = 1; j <= side; j++) {
-        var $newDiv = $('<div></div>');
-        $newDiv.addClass('normalDiv');
-        $newDiv.width(divLimit/side);
-        $newDiv.height(divLimit/side);
-        $('#container').append($newDiv);
-      }   
-      $('#container').append($('<br>'));
+function etch() {
+  'use strict';
+  $('.normalDiv').hover(function () {
+    $(this).addClass('hoverDiv');
+    $(this).removeClass('normalDiv');
+  },
+                        function () {
+  });
+}
+
+function generatePallet(side) {
+  'use strict';
+  var i, j, $newDiv, divLimit = 960;
+  $('body').prepend($('<div id="resize"><button>Change Pixel Density</button></div>'));
+  $('#resize').after($('<div id="container"></div>'));
+  for (i = 1; i <= side; i++) {
+    for (j = 1; j <= side; j++) {
+      $newDiv = $('<div></div>');
+      $newDiv.addClass('normalDiv');
+      $newDiv.width(divLimit / side);
+      $newDiv.height(divLimit / side);
+      $('#container').append($newDiv);
     }
+    $('#container').append($('<br>'));
   }
-
-  function clearPallet (side) {
-    $('#resize').empty();
-    $('#resize').remove();
-    $('#container').empty();
-    $('#container').remove();
-    generatePallet(side);
     etch();
     resize();
-  }
-  
+}
+
+function clearPallet (side) {
+  'use strict';
+  $('#resize').empty();
+  $('#resize').remove();
+  $('#container').empty();
+  $('#container').remove();
+  generatePallet(side);
+}
+
+function resize() {
+  'use strict';
+  $('#resize button').click(function () {
+    var newSide = parseInt(prompt("Specify the new width in squares you want to draw in. Remember, total area is width*height"), 10);
+    clearPallet(newSide);
+  });
+}
+
+$(document).ready(function () {
+  // Discovered that when things are dynamically made, any interactions on them must be put inside callable functions
+  'use strict';
+  generatePallet(16);
 });
